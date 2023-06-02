afedb6ff4178ba91f0772e7ec804ded491b935d9
ashmalzahra <ashmal.zahraa@gmail.com>
2023-06-03 02:07:48 +0500

    Style splash and devise views

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'categories/index'
+  get 'splash/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :categories, only: [:index, :new, :create] do
     resources :expenses, only: [:index, :new, :create]
   end
   # Defines the root path route ("/")
-   root "categories#index"
+   root "splash#index"
 end
