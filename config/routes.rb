167af9c451ba86515c5e6fa4fb359693c79244cd
ashmalzahra <ashmal.zahraa@gmail.com>
2023-06-03 04:15:25 +0500

    Style views

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'splash/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :categories, only: [:index, :new, :create] do
+  resources :categories, only: [:index, :new, :create, :show] do
     resources :expenses, only: [:index, :new, :create]
   end
   # Defines the root path route ("/")
